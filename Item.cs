2e552a3452dbd59df4d3883490fc2e95e9afa43c
Malka Bruk <malka2039@gmail.com>
2023-03-05 10:11:46 +0200

    connection string to local db

--- Item.cs
+++ Item.cs
@@ -7,7 +7,7 @@ public partial class Item
 {
     public int Id { get; set; }
 
-    public string Name { get; set; } = null!;
+    public string? Name { get; set; }
 
-    public bool IsComplete { get; set; }
+    public bool? IsComplete { get; set; }
 }
--- ToDoDbContext.cs
+++ ToDoDbContext.cs
@@ -18,19 +18,21 @@ public partial class ToDoDbContext : DbContext
     public virtual DbSet<Item> Items { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.22-mysql"));
+        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
-            .UseCollation("utf8_general_ci")
-            .HasCharSet("utf8");
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
 
         modelBuilder.Entity<Item>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.ToTable("items");
+
+            entity.Property(e => e.Name).HasMaxLength(45);
         });
 
         OnModelCreatingPartial(modelBuilder);
